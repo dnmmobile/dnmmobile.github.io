d9f78aa7ab2af8ba37867c2c9c4963b5140188c1
Hiep Tran <takiakun@gmail.com>
2019-12-22 22:25:31 +0700

    update pages

--- ppidesign/src/app/rank/rank.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-rank',
-  templateUrl: './rank.component.html',
-})
-export class RankComponent {
-  title = 'ppidesign';
-}
